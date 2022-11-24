e5abd8973c40ae882f20e47d801222a3d5624427
Babur Shah Sayer <92582416+BaburshahSayer@users.noreply.github.com>
2022-11-24 12:19:30 +0430

    Extended Deadline

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function() {
 
 
 $('#countdown').countdown({
-    date: "Nov 22 2022",
+    date: "Nov 30 2022",
     render: function(data) {
         var el = $(this.el);
         el.empty()
